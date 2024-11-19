191ed8d200954726709254b2273caa7b7192a579
SHENZA <shenza@oocl.com>
2024-11-19 16:17:17 +0800

    [MarsRover]refactor, refactor executeCommand function

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -6,19 +6,77 @@ public class MarsRover {
     private int y;
     private String direction;
 
+    public enum Direction {
+        N, E, S, W;
+    }
+
+    public MarsRover(){
+        this.x = 0;
+        this.y = 0;
+        this.direction = "N";
+    }
+
+    public MarsRover(int x, int y, String direction){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
     public String showStatus() {
         return x + ":" + y + ":" + direction;
     }
 
     public String executeCommand(String command) {
-        String direction = this.direction;
         if (command.equals("L")) {
-            direction = "W";
+            left();
 
         } else if (command.equals("R")) {
-            direction = "E";
+            right();
         }
-        this.direction = direction;
         return showStatus();
+    }
+
+    // 左转
+    public void left() {
+        switch (direction) {
+            case "N": this.direction = Direction.W.name(); break;
+            case "W": this.direction = Direction.S.name(); break;
+            case "S": this.direction = Direction.E.name(); break;
+            case "E": this.direction = Direction.N.name(); break;
+        }
+    }
+
+    // 右转
+    public void right() {
+        switch (direction) {
+            case "N": this.direction = Direction.E.name(); break;
+            case "E": this.direction = Direction.S.name(); break;
+            case "S": this.direction = Direction.W.name(); break;
+            case "W": this.direction = Direction.N.name(); break;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 }
